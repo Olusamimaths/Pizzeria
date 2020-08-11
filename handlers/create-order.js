e3abfd54eaafd36570b395af2eaa0eddea125271
Samuel Olusola <olusola.samuel.oluwatobi@gmail.com>
2020-08-11 12:31:09 +0100

    ✨ Making a delivery request before saving order

--- handlers/create-order.js
+++ handlers/create-order.js
@@ -1,28 +1,48 @@
 const AWS = require("aws-sdk");
+const rp = require("minimal-request-promise");
 const docClient = new AWS.DynamoDB.DocumentClient();
-const { v4: uuidv4 } = require("uuid");
 
 const createOrder = (order) => {
   if (!order || !order.pizza || !order.address)
     throw new Error("The order must have an id and a customer address.");
-  return docClient
-    .put({
-      TableName: "pizza-orders",
-      Item: {
-        orderId: uuidv4(),
-        pizza: order.pizza,
-        address: order.address,
-        orderStatus: "pending",
+
+  // make the delivery request before saving the order
+  return rp
+    .post("https://some-like-it-hot.effortless-serverless.com/delivery", {
+      headers: {
+        Authorization: "just-some-pizzzeria-api-344454",
+        "Content-type": "application/json",
       },
+      body: JSON.stringify({
+        pickupTime: "12: 21pm",
+        pickupAddress: "Some Pizzeria",
+        deliveryAddress: order.address,
+        webhookUrl:
+          "https://urdq9bteqa.execute-api.eu-central-1.amazonaws.com/latest/delivery",
+      }),
     })
-    .promise()
-    .then((res) => {
-      console.log("Order saved. ", res);
-      return res;
-    })
-    .catch((error) => {
-      console.log(`Error: order could not be saved,`, error);
-      throw error;
+    .then((rawResponse) => JSON.parse(rawResponse.body))
+    // save the order now
+    .then((response) => {
+      return docClient
+        .put({
+          orderId: response.deliveryId, // deliveryId is unique and can be used as id of order
+          TableName: "pizza-orders",
+          Item: {
+            pizza: order.pizza,
+            address: order.address,
+            orderStatus: "pending",
+          },
+        })
+        .promise()
+        .then((res) => {
+          console.log("Order saved. ", res);
+          return res;
+        })
+        .catch((error) => {
+          console.log(`Error: order could not be saved,`, error);
+          throw error;
+        });
     });
 };
 
